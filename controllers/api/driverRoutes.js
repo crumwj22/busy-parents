a1647c68803f01a5d0ffb9e9503a9e10c27647c0
crumwj22 <crumwj22@hotmail.com>
2022-06-04 12:35:38 -0700

    fixed homeroutes

--- controllers/api/driverRoutes.js
+++ controllers/api/driverRoutes.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
-const { Driver } = require('../../models');
+const { Driver, User, Rider } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const userData = await User.findAll({
+      //   include: [
+      //     {
+      //       model: Rider,
+      //       attributes: ['name'],
+      //     },
+      //   ],
+    });
+
+    // Serialize data so the template can read it
+    const user = userData.map((user) => user.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post('/', withAuth, async (req, res) => {
   try {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,12 +7,12 @@ router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const userData = await User.findAll({
-      include: [
-        {
-          model: Rider,
-          attributes: ['name'],
-        },
-      ],
+      // include: [
+      //   {
+      //     model: Rider,
+      //     attributes: ['name'],
+      //   },
+      // ],
     });
 
     // Serialize data so the template can read it
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
     // Pass serialized data and session flag into template
     res.render('homepage', {
       user,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -43,7 +43,7 @@ router.get('/user/:id', async (req, res) => {
 
     res.render('user', {
       ...user,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -64,7 +64,7 @@ router.get('/rider', withAuth, async (req, res) => {
 
     res.render('profile', {
       ...rider,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -83,7 +83,7 @@ router.get('/driver', withAuth, async (req, res) => {
 
     res.render('profile', {
       ...driver,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
