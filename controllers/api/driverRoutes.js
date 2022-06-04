a969811b2384598ae98cd74e10dce580b69ed1c8
crumwj22 <crumwj22@hotmail.com>
2022-06-04 11:48:33 -0700

    added driver route

--- controllers/api/driverRoutes.js
+++ controllers/api/driverRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Driver } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newDriver = await Driver.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newDriver);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const driverData = await Driver.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
+    if (!driverData) {
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(driverData);
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const driverRoutes = require('./driverRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/driver', driverRoutes);
 
 module.exports = router;
