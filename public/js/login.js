d571a3ebebbdb709f8c76a190ff3be850c26cf2a
crumwj22 <crumwj22@hotmail.com>
2022-06-08 18:54:45 -0700

    testing the connections

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/user/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/users');
+      document.location.replace('dashboard');
     } else {
       alert(response.statusText);
     }
