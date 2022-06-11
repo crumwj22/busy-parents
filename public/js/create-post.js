27d7d892115b1a8fd0bae8a7d622e3af054b2135
crumwj22 <crumwj22@hotmail.com>
2022-06-10 18:57:09 -0700

    minor changes to layouts

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,21 +1,25 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const pickup_Location = document.querySelector('#pickup_location').value.trim();
-  const dropoff_location = document.querySelector('#dropoff_location').value.trim();
+  const pickup_location = document
+    .querySelector('#pickup_location')
+    .value.trim();
+  const dropoff_location = document
+    .querySelector('#dropoff_location')
+    .value.trim();
   const availability = document.querySelector('#availability').value.trim();
 
-  if (pickup_Location && dropoff_location && availability) {
-    const response = await fetch(`/api/projects`, {
+  if (pickup_location && dropoff_location && availability) {
+    const response = await fetch(`/api/post`, {
       method: 'POST',
-      body: JSON.stringify({ pickup_Location, dropoff_location, availability }),
+      body: JSON.stringify({ pickup_location, dropoff_location, availability }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/homepage');
+      document.location.replace('/my-account');
     } else {
       alert('Failed to create driver post');
     }
