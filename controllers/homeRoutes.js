4a30b8247ab40ff008244c7d88c8fc93f986e266
AR31313 <gurlgotgame@gmail.com>
2022-06-11 08:59:34 -0700

    mydashboard route fixed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
       );
 
       // Pass serialized data into Handlebars.js template
-
+      console.log('test');
       res.render('members', { posts });
       return;
     } catch (err) {
@@ -36,41 +36,84 @@ router.get('/', async (req, res) => {
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-
+  console.log('test1');
   if (req.session.logged_in) {
-    res.render('members');
+    res.redirect('/dashboard');
     return;
   }
-
-  res.render('login');
+  console.log('test');
+  res.render('members');
 });
 
-// router.get('/my-account', async (req, res) => {
-//   if (req.session.user_id) {
-//     try {
-//       const commentData = await Comment.findAll({});
-//       //Issue
-//       console.log(commentData);
-//       const comments = commentData.map((comments) =>
-//         comments.get({ plain: true })
-//       );
-
-//       res.render('my-account', { comments });
-//       return;
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   }
-// });
 router.get('/my-account', async (req, res) => {
-  // Let the user in of they are already logged in. If Else, redirect the request login route.
+  if (req.session.user_id) {
+    try {
+      const commentData = await Comment.findAll({});
+      const comments = commentData.map((comments) =>
+        comments.get({ plain: true })
+      );
 
-  if (req.session.logged_in) {
-    res.redirect('members');
-    return;
+      res.render('my-account', { comments });
+      return;
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
+});
 
+module.exports = router;
+//HOMEPAGE
+router.get('/', async (req, res) => {
+  // If the user already has an account send them to the members page
+  console.log(req.session.user_id);
+  if (req.session.user_id) {
+    try {
+      const dbDriverData = await Driver.findAll({});
+      console.log(dbDriverData);
+      // Serialize user data so templates can read it
+      const posts = dbDriverData.map((driverpost) =>
+        driverpost.get({ plain: true })
+      );
+
+      // Pass serialized data into Handlebars.js template
+
+      res.render('members', { posts });
+      return;
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
   res.render('login');
 });
+
+
+
+router.get('/newpost', (req, res) => {
+  res.render('my-account', { logged_in: req.session.logged_in });
+});
+
+router.get('/singlepost/:id', async (req, res) => {
+  const postData = await Post.findByPk(req.params.id, {
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+      {
+        model: Comment,
+        include: [User],
+      },
+    ],
+  });
+
+  const post = postData.get({ plain: true });
+  const comments = post.comments;
+  res.render('singlePost', {
+    post,
+    comments,
+    logged_in: req.session.logged_in,
+  });
+});
+
 
 module.exports = router;
